23abd0c568663d39f662a1065384134f90802bfd
William Wong <compulim@users.noreply.github.com>
2025-12-31 11:58:56 +0000

    Revert

--- packages/on-error-resume-next/tsup.config.ts
+++ packages/on-error-resume-next/tsup.config.ts
@@ -4,7 +4,9 @@ export default defineConfig([
   {
     dts: true,
     entry: {
-      'on-error-resume-next': './src/index.ts'
+      'on-error-resume-next': './src/index.ts',
+      'on-error-resume-next.async': './src/index.async.ts',
+      'on-error-resume-next.auto': './src/index.auto.ts'
     },
     format: ['cjs', 'esm'],
     sourcemap: true,
